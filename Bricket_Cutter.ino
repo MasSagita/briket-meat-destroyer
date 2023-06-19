abc42478105d5dacb7e5c019abefb9d4618aca60
MasSagita <40248554+MasSagita@users.noreply.github.com>
2023-06-19 11:57:10 +0700

    Add files via upload

---
+++ Bricket_Cutter.ino
@@ -0,0 +1,235 @@
+#include <EEPROM.h>
+
+#include "input_output.h"
+
+void (* resetFunc) (void) = 0;
+
+int last_counter_cut;
+
+//dummy variable
+int con = 0, cut = 1;
+
+//speed motor 1 and 2
+int con_speed, cut_speed;
+
+//delay
+int cut_delay;
+int set_delay;
+
+void setup() {
+  // put your setup code here, to run once:
+  cut_speed = EEPROM.read(0);
+  con_speed = EEPROM.read(1);
+  set_delay = EEPROM.read(2);
+
+  Serial.begin(115200);
+
+  setup_input_output();
+
+  Serial.println("Bricket Cutter");
+
+  standby();
+}
+
+int cutter_state;
+
+int cutter_test;
+
+unsigned long prev_millis_con;
+
+int start_grinder;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led_pin, !digitalRead(led_pin));
+  refresh_screen(5);
+  run_system();
+  //counter cut 
+  last_counter_cut = total_cut;
+}
+
+void repost_motor_cutter() {
+  while (!ls_atas()) {
+    run_motor(cut, -cut_speed, 1);
+    Serial.print("Repost Cutter ");
+    Serial.println(cut_speed);
+    if (ls_atas()) {
+      run_motor(cut, 0, 1);
+      break;
+    }
+  }
+}
+
+unsigned long current_millis_con;
+
+void run_system() {
+  current_millis_con = millis();
+
+  Serial.println(current_millis_con - prev_millis_con);
+  lcd.setCursor(0, 0), lcd.print(F(" Extract Briket"));
+  lcd.setCursor(0, 1), lcd.print(current_millis_con - prev_millis_con), lcd.print(F("ms"));
+  lcd.setCursor(8, 1), lcd.print(total_cut);
+  
+  if ((cutter_state == 0) && current_millis_con - prev_millis_con >= cut_delay) {
+    Serial.println("Cut Briket");
+    run_motor(con, 0, 1), grinder_off;
+    run_cutter();
+    cutter_test = 1; // test menu
+    cutter_state = 1;
+    //prev_millis_con = current_millis_con;
+  }
+  else if (cutter_state == 1) {
+    if (button(2)) standby();
+    cutter_test = 0;
+    Serial.println("Extract Briket");
+    grinder_on;
+    run_motor(con, con_speed, 1);
+    cutter_state = 0;
+    
+    prev_millis_con = current_millis_con;
+  }
+}
+
+void run_cutter() {
+  run_motor(cut, cut_speed, 1);
+  beep_buz(1, 50);
+  lcd.clear();
+  while (1) {
+    counter_cut(ls_bawah());
+    lcd.setCursor(0, 0), lcd.print(F("    Cutting"));
+    if (ls_bawah()) run_motor(cut, -cut_speed, 1);
+    if (ls_atas()) {
+      lcd.clear();
+      lcd.setCursor(0, 0), lcd.print(F("  Done Cutting"));
+      run_motor(cut, 0, 1);
+      beep_buz(1, 20);
+      break;
+    }
+  }
+}
+
+int set_num = 0;
+
+void standby() {
+  lcd.clear();
+  lcd.setCursor(0, 0), lcd.print(F("     Repost"));
+  lcd.setCursor(0, 1), lcd.print(F("  Motor Cutter"));
+  repost_motor_cutter();
+  delay(500);
+  lcd.clear();
+  lcd.setCursor(0, 0), lcd.print(F("    Standby!"));
+  Serial.println("Standby");
+  beep_buz(1, 500);
+  set_num = 0;
+  while (1) {
+    refresh_screen(5);
+    if (button(2)) set_num += 1, beep_buz(1, 50);
+    if (set_num > 4) set_num = 0;
+
+    if (set_num == 0) {
+      //change speed motor
+      if (button(0)) set_delay += 1, beep_buz(1, 25);
+      if (button(1)) set_delay -= 1, beep_buz(1, 25);
+
+      set_delay = limitValue(set_delay, 0, 70);
+
+      cut_delay = set_delay * 100;
+
+      if (btn_test(0)) {
+        lcd.clear();
+        beep_buz(2, 100);
+        cutter_state = HIGH;
+        cutter_test = 0;
+        while (1) {
+          run_system();
+          if (cutter_test) {
+            lcd.clear(), beep_buz(1, 300);
+            break;
+          }
+        }
+      }
+      lcd.setCursor(0, 1), lcd.write(0); 
+      lcd.setCursor(1, 0), lcd.print(F("Delay Cutter"));
+      lcd.setCursor(1, 1), lcd.print(cut_delay), lcd.print(F("ms"));
+    }
+
+    if (set_num == 1) {
+      lcd.setCursor(1, 0), lcd.print(F("Done?"));      
+      lcd.setCursor(9, 0), lcd.print(F("TC:")), lcd.print(last_counter_cut);
+      lcd.setCursor(0, button(0)), lcd.write(0);
+      if (button(0)) {
+        lcd.clear();
+        lcd.setCursor(0, 1), lcd.print(F("    Running"));
+        total_cut = last_counter_cut;
+        beep_buz(4, 55);
+        cutter_state = HIGH;
+        break;
+      }
+    }
+
+    if (set_num == 2) {
+      //change speed motor
+      if (button(0)) cut_speed += 1, beep_buz(1, 25);
+      if (button(1)) cut_speed -= 1, beep_buz(1, 25);
+      //pwm motor limitation
+      cut_speed = limitValue(cut_speed, 60, 250);
+
+      if (btn_test(0)) {
+        run_cutter();
+      }
+      lcd.setCursor(0, 1), lcd.write(0);
+      lcd.setCursor(1, 0), lcd.print(F("Spd Cut "));
+      lcd.setCursor(1, 1), lcd.print(cut_speed), lcd.print(F("pwm"));
+    }
+
+    if (set_num == 3) {
+      //change speed motor
+      if (button(0)) con_speed += 1, beep_buz(1, 25);
+      if (button(1)) con_speed -= 1, beep_buz(1, 25);
+      //pwm motor limitation
+      con_speed = limitValue(con_speed, 50, 250);
+
+      if (btn_test(0)) {
+        run_motor(con, con_speed, 1), grinder_on;
+      }
+      else run_motor(con, 0, 1), grinder_off;
+      lcd.setCursor(0, 1), lcd.write(0);
+      lcd.setCursor(1, 0), lcd.print(F("Spd Con ")), lcd.print(btn_test(0));
+      lcd.setCursor(1, 1), lcd.print(con_speed), lcd.print(F("pwm"));
+    }
+
+    if (set_num == 4) {
+      lcd.setCursor(1, 0), lcd.print(F("Save?"));
+      lcd.setCursor(0, button(0)), lcd.write(0);
+      if (button(0)) save();
+    }
+    lcd.setCursor(13, 1), lcd.print(ls_atas());
+    lcd.setCursor(15, 1), lcd.print(ls_bawah());
+  }
+}
+
+// Save and auto reset software
+void save() {
+  beep_buz(2, 400);
+  delay(500), lcd.clear();
+  lcd.setCursor(0, 0), lcd.print("  Save & Rstrt");
+  EEPROM.write(0, cut_speed);
+  EEPROM.write(1, con_speed);
+  EEPROM.write(2, set_delay);
+  for (int i = 0; i < 16; i++) {
+    lcd.setCursor(i, 1), lcd.print(F("."));
+    delay(50);
+  }
+  resetFunc(); // reset microcontroller
+}
+
+int limitValue(int value, int minValue, int maxValue) {
+  if (value < minValue) {
+    value = minValue;
+  } else if (value > maxValue) {
+    value = maxValue;
+  }
+  return value;
+}
+
+
---
+++ input_output.h
@@ -0,0 +1,145 @@
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+//draw the arrow
+byte customChar[] = {
+  B00000,
+  B00100,
+  B00010,
+  B11111,
+  B00010,
+  B00100,
+  B00000,
+  B00000
+};
+
+//button pin up down and select
+int btn_pin[3] = {2, 3, 4};
+
+int ls_pin[2] = {A0, A1};
+
+const int led_pin = 13;
+const int buz_pin = 12;
+
+//Motor driver pin
+const int motor_pwm_pin[2] = {9, 6};
+const int motor_left_pin[2] = {11, 7};
+const int motor_right_pin[2] = {10, 8};
+
+// test button
+int btn_test_pin[2] = {A2, A3};
+
+//relay grinder
+const int grinder = 5;
+
+#define grinder_off digitalWrite(grinder, LOW);
+#define grinder_on digitalWrite(grinder, HIGH);
+
+//input and output setup
+void setup_input_output() {
+  lcd.init();
+
+  pinMode(grinder, OUTPUT);
+
+  digitalWrite(grinder, LOW);
+  
+  for (int i = 0; i < 3; i++) {
+    pinMode(btn_pin[i], INPUT);
+  }
+
+  for (int i = 0; i < 2; i++) {
+    pinMode(btn_test_pin[i], INPUT_PULLUP);
+    pinMode(ls_pin[i], INPUT_PULLUP);
+    pinMode(motor_pwm_pin[i], OUTPUT);
+    pinMode(motor_left_pin[i], OUTPUT);
+    pinMode(motor_right_pin[i], OUTPUT);
+  }
+  
+  pinMode(led_pin, OUTPUT);
+  pinMode(buz_pin, OUTPUT);
+  
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(buz_pin, !digitalRead(buz_pin));
+    digitalWrite(led_pin, !digitalRead(led_pin));
+    delay(30);
+  }
+  
+  lcd.createChar(0, customChar);
+  lcd.backlight();
+  lcd.setCursor(0, 0), lcd.print(F(" Bricket Cutter"));
+  delay(1000);
+  lcd.clear();
+}
+
+// run motor function
+void run_motor(int ch, int spd, int brk) {
+  if (spd == 0 && brk == 1) {
+    analogWrite(motor_pwm_pin[ch], 255);
+    digitalWrite(motor_left_pin[ch], 0);
+    digitalWrite(motor_right_pin[ch], 0);
+  }
+  if (spd == 0 && brk == 0) {
+    analogWrite(motor_pwm_pin[ch], 0);
+    digitalWrite(motor_left_pin[ch], 0);
+    digitalWrite(motor_right_pin[ch], 0);
+  }
+  if (spd > 0) {
+    analogWrite(motor_pwm_pin[ch], spd);
+    digitalWrite(motor_left_pin[ch], 1);
+    digitalWrite(motor_right_pin[ch], 0);
+  }
+  if (spd < 0) {
+    analogWrite(motor_pwm_pin[ch], -spd);
+    digitalWrite(motor_left_pin[ch], 0);
+    digitalWrite(motor_right_pin[ch], 1);
+  }
+}
+
+//button reading function
+bool button(int ch) {
+  return !digitalRead(btn_pin[ch]); 
+}
+
+bool btn_test(int ch) {
+  return !digitalRead(btn_test_pin[ch]);
+}
+
+bool ls_atas() {
+  return !digitalRead(ls_pin[0]);
+}
+
+bool ls_bawah() {
+  return !digitalRead(ls_pin[1]);
+}
+
+//ls counter function
+int last_state_ls = HIGH;
+int ls_counter = 0;
+int total_cut = 0;
+void counter_cut(int pin) {
+  if (!last_state_ls && pin) {
+    ls_counter += 1;
+    total_cut += 1;
+  }
+  last_state_ls = pin;
+}
+
+//beep buzzer
+void beep_buz(int sum, int interval) {
+  if (sum > 0) sum = sum * 2;
+  if (interval >= 500) interval = 500;
+  for (int i = 0; i < sum; i++) {
+    digitalWrite(buz_pin, !digitalRead(buz_pin));
+    delay(interval);
+  }
+}
+
+//refresh lcd screen
+int refresh;
+void refresh_screen(int intervalRefresh) {
+  if (++refresh > intervalRefresh) {
+    lcd.clear();
+    refresh = 0;
+  }
+}
+
